aa9f4c47957c170a610c8a1a185947b513bf2c41
Ayan6174877 <68985427+Ayan6174877@users.noreply.github.com>
2020-09-14 13:15:47 +0530

    Add files via upload

--- factorial/factorial.js
+++ factorial/factorial.js
@@ -3,24 +3,24 @@
 // foctorial of 5 = 5 * 4 * 3 * 2 * 1 = 120
 
 // using arror function and using recursive function
-//  let factorial = (val) =>{ 
-//     if (val == 0){
-//         return 1;
-//     }
-//     else{
-//         return val * factorial(val - 1)
-//     }
-// } 
+ let factorial = (val) =>{ 
+    if (val == 0){
+        return 1;
+    }
+    else{
+        return val * factorial(val - 1)
+    }
+} 
 
-// using normal function and loops
-let factvalue = 1;
-function factorial(val){
-   while (val >= 1){
-       factvalue = factvalue * val;
-       val--;
-   }
-   return factvalue;
-}
+// // using normal function and loops
+// let factvalue = 1;
+// function factorial(val){
+//    while (val >= 1){
+//        factvalue = factvalue * val;
+//        val--;
+//    }
+//    return factvalue;
+// }
 
 FactorialNumber = factorial(3);
 document.getElementById("app").innerHTML = FactorialNumber;
---
+++ function_declarations.js
@@ -0,0 +1,22 @@
+// function can be mostly defined in three types
+
+// 1 normal function
+
+function sum(a, b){
+    let result = a + b;
+    return result;
+}
+
+// 2 binding function
+
+const sun = function(){
+    let result = a + b;
+    return result;
+}
+
+// 3 arrow functions
+
+sum = (a, b) => {
+    let result = a + b;
+    return result;
+};
